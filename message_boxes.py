7a645cc090feb599ebeb143d7cefd8ed8e14ab0d
Steve Brenneis <swbrenneis@foronlyyou.com>
2025-10-08 20:56:15 -0400

    Finished coding

--- message_boxes.py
+++ message_boxes.py
@@ -25,6 +25,55 @@ class MainWindow(QWidget):
         catalogue_label.setFont(QFont('Arial', 18))
 
         search_label = QLabel("Search Results", self)
+        search_label.move(20, 40)
+
+        # Create author QLabel and QLineEdit widgets
+        author_label = QLabel("Name:", self)
+        author_label.move(20, 74)
+
+        self.author_edit = QLineEdit(self)
+        self.author_edit.move(70, 70)
+        self.author_edit.resize(240, 24)
+        self.author_edit.setPlaceholderText("Enter names as: First Last")
+
+        # Create the search QPushButton
+        search_button = QPushButton("Search", self)
+        search_button.move(140, 100)
+        search_button.clicked.connect(self.searchAuthors)
+
+    def searchAuthors(self):
+        """ Search through a catalog of names.
+        If a name is found, display the author found dialog.
+        Otherwise, display Author Not FOund dialog"""
+        file = "files/authors.txt"
+
+        try:
+            with open(file, "r") as f:
+                authors = [line.rstrip("\n") for line in f]
+
+            # Check for name in authors list
+            if self.author_edit.text() in authors:
+                QMessageBox.information(self, "Author Found",
+                                        "Author found in catalogue!",
+                                        QMessageBox.StandardButton.Ok)
+            else:
+                answer = QMessageBox.question(self, "Author Not Found",
+                                              """<p>Author not found in catalogue.</p>
+                                              <p>Do you wish to continue?</p>""",
+                                              QMessageBox.StandardButton.Yes |\
+                                              QMessageBox.StandardButton.No,
+                                              QMessageBox.StandardButton.No)
+
+                if answer == QMessageBox.StandardButton.No:
+                    print("Closing application")
+                    self.close()
+        except FileNotFoundError as error:
+            QMessageBox.warning(self, "Error",
+                                f"""<p>File not found.</p>
+                                <p>Error: {error}</p>
+                                Closing application.""",
+                                QMessageBox.StandardButton.Ok)
+            self.close()
 
 
 if __name__ == "__main__":
