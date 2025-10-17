d6c67e4cd1988b0fe82db4e6236fe49d7c224979
Steve Brenneis <swbrenneis@foronlyyou.com>
2025-10-16 22:50:44 -0400

    Initial commit

---
+++ bizza/src/Dashboard.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+const Dashboard = () => {
+    const  [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <>
+            {isLoggedIn ? (
+                <button onClick={handleLogout}>Logout</button>
+            ) : (
+                <button onClick={handleLogin}>Login</button>
+            )}
+        </>
+    );
+}
+
+export default Dashboard;
